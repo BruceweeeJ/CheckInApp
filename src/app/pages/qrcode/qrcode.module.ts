d97aa92bb76803a33a06a2b70b8068f42d6c1216
BruceweeeJ <18506904188@163.com>
2019-04-14 20:32:14 +0800

    qrcode界面添加

---
+++ src/app/pages/qrcode/qrcode.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { QrcodePage } from './qrcode.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: QrcodePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [QrcodePage]
+})
+export class QrcodePageModule {}
