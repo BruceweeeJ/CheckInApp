1ac91d20ee31d598e01c9cb85ab2606b55cac73b
BruceweeeJ <18506904188@163.com>
2019-06-18 11:04:29 +0800

    关于我们完成 设置初步

--- src/app/pages/setting/setting.page.ts
+++ src/app/pages/setting/setting.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController} from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-setting',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingPage implements OnInit {
 
-  constructor() { }
+  constructor(
+      private alertController: AlertController,
+      private toast: ToastController,
+      private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+  async onUpdate() {
+      const toast = await this.toast.create({
+          message: '已是最新版本!',
+          duration: 1500
+      });
+      return await toast.present();
+  }
+  onLogout() {
+    this.router.navigateByUrl('/login');
   }
 
 }
